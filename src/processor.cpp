03e4f38a966609c2f436f3dacbff66e89074e38a
iconrealestate77 <iconrealestate77@gmail.com>
2026-09-02 23:11:20 +0400

    Add CPU utilization calculation in Processor class
    Implement CPU utilization calculation using active and idle jiffies.

--- src/processor.cpp
+++ src/processor.cpp
@@ -1,4 +1,14 @@
 #include "processor.h"
+#include "linux_parser.h"
 
-// TODO: Return the aggregate CPU utilization
-float Processor::Utilization() { return 0.0; }
+float Processor::Utilization() {
+  static long prevActive = 0, prevIdle = 0;
+  long active = LinuxParser::ActiveJiffies();
+  long idle = LinuxParser::IdleJiffies();
+  long totalDelta = (active + idle) - (prevActive + prevIdle);
+  long activeDelta = active - prevActive;
+  prevActive = active;
+  prevIdle = idle;
+  if (totalDelta <= 0) return 0.0;
+  return static_cast<float>(activeDelta) / static_cast<float>(totalDelta);
+}
